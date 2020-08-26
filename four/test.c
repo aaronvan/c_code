43de65f113d92737894c728466d86b8c3cb51d14
Aaron <aaronvan@icloud.com>
2020-08-26 12:52:48 -0700

    new chapter

---
+++ four/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    int height = 73;
+    printf("I am %d inches tall.\n", height);
+
+    for (int i = 0; i <= 10; i++) {
+	printf("The number: %d\n", i);
+    }
+
+    return EXIT_SUCCESS;
+}
