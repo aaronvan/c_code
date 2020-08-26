608a5007f2de3c87f6d4b2b447bf0c4242eadfc5
Aaron <aaronvan@icloud.com>
2020-08-26 14:02:07 -0700

    update ex8

--- eight/ex8.c
+++ eight/ex8.c
@@ -5,8 +5,9 @@ int main(int argc, char *argv[]) {
     if (argc == 1) {
 	printf("You didn't have any arguments.\n");
     } else {
+      printf("You entered %d arguments.\n", argc);
       printf("Here are your arguments:\n");
-	    for (int i = 0; i < argc; i++) {
+	    for (int i = 1; i < argc; i++) {
 		printf("%s ", argv[i]);
 	    }
 	    printf("\n");
