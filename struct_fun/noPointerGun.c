57126cc507825dc7e20b0888707169f6795d1715
Aaron <aaronvan@icloud.com>
2020-08-31 10:33:43 -0700

    no typedef

--- struct_fun/noPointerGun.c
+++ struct_fun/noPointerGun.c
@@ -3,14 +3,14 @@
 #include <string.h>
 #include <assert.h>
 
-typedef struct Gun {
+struct Gun {
     char *name;
     int calibre;
     int range;
-} gun_t;
+};
 
-gun_t gunMake(char *name, int calibre, int range) {
-    gun_t gun;
+struct Gun gunMake(char *name, int calibre, int range) {
+    struct Gun gun;
 
     gun.name = name;
     gun.calibre = calibre;
@@ -19,14 +19,14 @@ gun_t gunMake(char *name, int calibre, int range) {
     return gun;
 }
 
-void gunPrint(gun_t gun) {
+void gunPrint(struct Gun gun) {
     printf("Model: %s\n", gun.name);
     printf("Calibre: %d\n", gun.calibre);
     printf("Range: %d\n", gun.range);
 }
 
 int main(void) {
-    gun_t paladin = gunMake("Super Paladin", 155, 25000);
+    struct Gun paladin = gunMake("Super Paladin", 155, 25000);
     gunPrint(paladin);
 
     return 0;
