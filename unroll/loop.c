2f9592798f67c41ce459d528585ae543dfc5ffc0
Aaron <aaronvan@icloud.com>
2020-09-02 10:55:50 -0700

    update loop unroller

--- unroll/loop.c
+++ unroll/loop.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
+
     puts("First a normal loop:");
+    printf("\n");
     int x;
     for (x = 0; x < 100; x++) {
-	printf("%d\n", x);
+	printf("%d ", x);
     }
+    printf("\n\n");
 
-    puts("Now unroll the loop.");
+    puts("Now unroll the loop:");
+    printf("\n");
+
     int y;
     for (y = 0; y < 100; y += 5) {
 	printf("%d ", y);
@@ -15,8 +20,9 @@ int main(void) {
 	printf("%d ", y + 2);
 	printf("%d ", y + 3);
 	printf("%d ", y + 4);
-	printf("\n");
     }
+
+    printf("\n");
 
     return 0;
 }
