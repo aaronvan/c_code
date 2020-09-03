5d7308fa03a3d22c5e5c3a613ad31a0e1eadb919
Aaron <aaronvan@icloud.com>
2020-09-03 11:29:07 -0700

    generic pointers

---
+++ Huw/generic/gen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int stuff[] = {88, 44, 87};
+    void *gp;
+
+    // point the pointer at the array
+    gp = stuff;
+
+    // cast the pointer as an int and dereference it.
+    printf("The item pointed to by gp is %d\n", *(int*)gp);
+
+    return 0;
+}
