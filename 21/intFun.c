f30f019ca12a03d86f71d87668b54018aa0220a3
Aaron <aaronvan@icloud.com>
2020-09-10 11:07:17 -0700

    enums

--- 21/intFun.c
+++ 21/intFun.c
@@ -4,6 +4,10 @@
 
 int main(void) {
 
+    const int x = 88;
+    printf("%d is a constant.\n", x);
+    // error: x += 1;
+
     /* exact data types */
     int8_t a = -1;
     uint8_t b = 1;
@@ -14,6 +18,7 @@ int main(void) {
     int64_t g = -444444444;
     uint64_t h = 444444444;
     printf("%d, %d\n %d, %d\n %d, %d\n %d, %d\n", a, b, c, d, e, f, g, h);
+    printf("%x, %x\n %x, %x\n %x, %x\n %x, %x\n", a, b, c, d, e, f, g, h);
 
     /* enum example */
     enum chuck { Sunday, Monday, Tuesday, Wednesday,
